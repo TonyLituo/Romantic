861314f0d8ef70bcef8e3bde99f21eacb08c41ce
TonyLituo <1439804563@qq.com>
2019-06-20 00:13:51 +0800

    Mvp初步实现

--- app/src/main/java/com/lt/app/MainActivity.java
+++ app/src/main/java/com/lt/app/MainActivity.java
@@ -1,13 +1,38 @@
 package com.lt.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.lt.app.presenter.NormalPresenter;
+import com.lt.app.view.INormaView;
+import com.lt.mvp.MvpActivity;
+
+public class MainActivity extends MvpActivity<INormaView, NormalPresenter> implements INormaView, View.OnClickListener {
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = this.findViewById(R.id.tv);
+        mTextView.setOnClickListener(this);
+    }
+
+    @Override
+    protected NormalPresenter createPresenter() {
+        return new NormalPresenter();
+    }
+
+
+    @Override
+    public void showToast(String content) {
+        Toast.makeText(this, content, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        mPresenter.login("zhangsan", "1234");
     }
 }
